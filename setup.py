dc4f46d16ab71fffea26156eb631f165c4e6ab31
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2014-11-20 00:03:21 +0100

    release: 0.2.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'nosql-rest-preprocessor',
   packages = ['nosql_rest_preprocessor'],
-  version = '0.1',
+  version = '0.2.1',
   description = 'A middleware module which solves common problems when building rest-apis with nosql databases.',
   author = 'Felix Bruckmeier',
   author_email = 'felix.m.bruckmeier@gmail.com',
   url = 'https://github.com/felixbr/nosql-rest-preprocessor/',
-  download_url = 'https://github.com/felixbr/nosql-rest-preprocessor/tarball/0.1',
+  download_url = 'https://github.com/felixbr/nosql-rest-preprocessor/tarball/0.2.1',
   keywords = ['nosql', 'rest', 'web', 'middleware'],
   classifiers = [],
 )
